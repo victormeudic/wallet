d00e10e48b668c221514cd58e4e47cf577ef675d
Victor Meudic <victormeudic@gmail.com>
2023-02-15 18:54:21 +0100

    v1

---
+++ hardhat.config.js
@@ -0,0 +1,24 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: "0.8.9"
+  },
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337
+    }
+  }
+};
--- src/App.js
+++ src/App.js
@@ -1,23 +1,116 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
+import Wallet from './artifacts/contracts/Wallet.sol/Wallet.json';
 import './App.css';
 
+let WalletAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
 function App() {
+
+  const [balance, setBalance] = useState(0);
+  const [amountSend, setAmountSend] = useState();
+  const [amountWithdraw, setAmountWithdraw] = useState();
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+
+  useEffect(() => {
+    getBalance();
+  }, [])
+
+  async function getBalance() {
+    if(typeof window.ethereum !== 'undefined') {
+      const accounts = await window.ethereum.request({method:'eth_requestAccounts'});
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(WalletAddress, Wallet.abi, provider);
+      try {
+        let overrides = {
+          from: accounts[0]
+        }
+        const data = await contract.getBalance(overrides);
+        setBalance(String(data));
+      }
+      catch(err) {
+        setError('Une erreur est survenue.');
+      }
+    }
+  }
+
+  function changeAmountSend(e) {
+    setAmountSend(e.target.value);
+  }
+
+  async function transfer() {
+    if (!amountSend) {
+      return;
+    }
+    setError('');
+    setSuccess('');
+    if (typeof window.ethereum !== 'undefined') {
+      const accounts = await window.ethereum.request({method:'eth_requestAccounts'});
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      try {
+        const tx = {
+          from: accounts[0],
+          to: WalletAddress,
+          value: ethers.utils.parseEther(amountSend)
+        }
+        const transaction = await signer.sendTransaction(tx);
+        await transaction.wait();
+        setAmountSend('');
+        getBalance();
+        setSuccess('Votre argent a bien été transféré sur le portefeuille !')
+      }
+      catch (err) {
+        setError('Une erreur est survenue');
+      }
+    }
+  }
+
+  function changeAmountWithdraw(e) {
+    setAmountWithdraw(e.target.value);
+  }
+
+  async function withdraw() {
+    if (!amountWithdraw) {
+      return;
+    }
+    setError('');
+    setSuccess('');
+    const accounts = await window.ethereum.request({method:'eth_requestAccounts'});
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(WalletAddress, Wallet.abi, signer);
+    try {
+      const transaction = await contract.withdrawMoney(accounts[0], ethers.utils.parseEther(amountWithdraw));
+      await transaction.wait();
+      setAmountWithdraw('');
+      getBalance();
+      setSuccess('Votre argent a bien été retiré du portefeuille');
+    }
+    catch (err) {
+      setError('Une erreu est survenue');
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+      {error && <p className='error'>{error}</p>}
+      <h2>{balance / 10**18} eth</h2>
+      <div className='wallet_flex'>
+        <div className='walletG'>
+          <h3>Envoyer de l'ether</h3>
+          <input type="text" placeholder='Montant en Ether' onChange={changeAmountSend} />
+          <button onClick={transfer}>Envoyer</button>
+        </div>
+        <div className='walletD'>
+          <h3>Retirer de l'ether</h3>
+          <input type="text" placeholder='Montant en Ether' onChange={changeAmountWithdraw} />
+          <button onClick={withdraw}>Retirer</button>
+        </div>
+      </div>
+      </div>
     </div>
   );
 }
